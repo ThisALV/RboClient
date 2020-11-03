dc2bfd299fbd6cdc8c43195864f6f88a142dc377
ThisALV <lel.charriere@orange.fr>
2020-11-03 20:28:32 +0100

    Ajout de la barre de séparation entre infos du joueur et hôte. Ajouts des champs pour les infos du joueur. HomeOption pour généraliser les cases à cacher du formulaire. HomeInputRow pour généraliser les suites de zones de texte du formulaire. PortInput -> NumericInput pour généraliser les zones de texte prenant un entier naturel d'un certain nombre de chiffres en entrée.

--- rboclient/gui/home.py
+++ rboclient/gui/home.py
@@ -1,19 +1,41 @@
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
+from kivy.properties import NumericProperty, StringProperty, BooleanProperty
 
 
 class HomeInput(TextInput):
     pass
 
 
+class HomeInputRow(BoxLayout):
+    pass
+
+
+class HomeOption(BoxLayout):
+    label = StringProperty()
+    enabled = BooleanProperty(False)
+
+
+class HostInput(HomeInputRow):
+    address = StringProperty()
+    port = StringProperty()
+
+
+class PlayerInput(HomeInputRow):
+    playerID = StringProperty()
+    name = StringProperty()
+
+
 class LocalhostOption(BoxLayout):
     pass
 
 
-class PortInput(HomeInput):
+class NumericHomeInput(HomeInput):
+    digits = NumericProperty()
+
     def insert_text(self, substr: str, from_undo: bool = False):
-        if len(self.text) + len(substr) > 5:
+        if len(self.text) + len(substr) > self.digits:
             return
 
         for c in substr:
