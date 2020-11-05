39f0da859189bb4acef628866a9592143b60a9ac
ThisALV <lel.charriere@orange.fr>
2020-11-05 19:19:33 +0100

    Émission de on_login, traité par ClientApp. Limite de valeur numérique pour NumericHomeInput. Vérification de la validité des champs.

--- rboclient/gui/home.py
+++ rboclient/gui/home.py
@@ -5,8 +5,22 @@ from kivy.properties import NumericProperty, StringProperty, BooleanProperty, Ob
 from kivy.event import EventDispatcher
 from kivy.logger import Logger
 
+from math import inf
+
 
 class HomeInput(TextInput):
+    defaultForeground = [1, 1, 1, 1]
+    invalidForeground = [.7, 0, 0, 1]
+
+    defaultHint = [.7, .7, .7, 1]
+    invalidHint = [.5, 0, 0, 1]
+
+    def __init__(self, **kwargs):
+        super().__init__(foreground_color=HomeInput.defaultForeground, hint_text_color=HomeInput.defaultHint, **kwargs)
+
+    def valid(self) -> bool:
+        return len(self.text) != 0
+
     def on_text_validate(self):
         self.parent.dispatch("on_validate")
 
@@ -15,6 +29,25 @@ class HomeInputRow(BoxLayout):
     def __init__(self, **kwargs):
         self.register_event_type("on_validate")
         super().__init__(**kwargs)
+
+    def makeInvalid(self, id: str) -> None:
+        target = self.ids[id]
+
+        def disable(_: EventDispatcher, __: str):
+            target.foreground_color = HomeInput.defaultForeground
+            target.hint_text_color = HomeInput.defaultHint
+
+        target.foreground_color = HomeInput.invalidForeground
+        target.hint_text_color = HomeInput.invalidHint
+
+        target.bind(text=disable)
+
+    def check(self, id: str) -> bool:
+        valid = self.ids[id].valid()
+        if not valid:
+            self.makeInvalid(id)
+
+        return valid
 
     def on_validate(self):
         pass
@@ -41,6 +74,10 @@ class LocalhostOption(BoxLayout):
 
 class NumericHomeInput(HomeInput):
     digits = NumericProperty()
+    max = NumericProperty(+inf)
+
+    def valid(self) -> bool:
+        return super().valid() and int(self.text) <= self.max
 
     def insert_text(self, substr: str, from_undo: bool = False):
         if len(self.text) + len(substr) > self.digits:
@@ -58,10 +95,23 @@ class Home(AnchorLayout):
     playerInput = ObjectProperty()
 
     def __init__(self, **kwargs):
+        self.register_event_type("on_login")
         super().__init__(**kwargs)
 
         for inputRow in [self.hostInput, self.playerInput]:
             inputRow.bind(on_validate=self.login)
 
     def login(self, _: EventDispatcher = None):
+        valid = True
+
+        for (row, inputs) in [(self.hostInput, ["address", "port"]), (self.playerInput, ["playerID", "name"])]:
+            for field in inputs:
+                valid = row.check(field) and valid
+
+        if valid:
+            self.dispatch("on_login",
+                          host=(self.hostInput.address, self.hostInput.port),
+                          player=(self.playerInput.playerID, self.playerInput.name))
+
+    def on_login(self, **_):
         Logger.debug("Home : Connexion...")
