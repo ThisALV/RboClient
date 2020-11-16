a87820db0b509417ba45fea47e60ca7b99c85652
ThisALV <lel.charriere@orange.fr>
2020-11-16 21:52:47 +0100

    Une configuration par défaut est créée si config.ini n'est pas présent.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from kivy.config import Config
 
+from os import path
+
 Config.set("graphics", "borderless", 1)
 Config.set("kivy", "exit_on_escape", 0)
 
@@ -7,6 +9,24 @@ Config.set("graphics", "position", "custom")
 Config.set("graphics", "top", 1250)
 Config.set("graphics", "left", 600)
 Config.set("kivy", "log_level", "debug")
+
+cfgFile = "config.ini"
+
+defaultCfg = """
+[Host]
+address=
+port=
+
+[Player]
+id=
+name=
+"""
+
+if not path.isfile(cfgFile):
+    with open(cfgFile, mode="w") as config:
+        config.write(defaultCfg)
+
+Config.read(cfgFile)
 
 import kivy.resources  # noqa E402
 from rboclient.gui.app import ClientApp  # noqa E402
